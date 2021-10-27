7bcccb5403aff80c073c4b590dcd3160f2276498
Alex G <test@gmail.com>
2021-10-27 13:50:58 +0300

    add day 7

---
+++ src/main/java/day7/Player.java
@@ -0,0 +1,60 @@
+package day7;
+
+public class Player {
+    public static final int MAX_STAMINA = 100;
+    public static final int MIN_STAMINA = 0;
+
+    public static int countPlayers = 0;
+
+    private int stamina;
+
+    public Player(int stamina) {
+        this.stamina = stamina;
+        if (countPlayers < 6) {
+            countPlayers++;
+        }
+    }
+
+    public static int getCountPlayers() {
+        return countPlayers;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void run() {
+        if (stamina > MIN_STAMINA) {
+            stamina--;
+            if (stamina == MIN_STAMINA) {
+                countPlayers--;
+            }
+        } else {
+            System.out.println("Тебе нужно отдохнуть!");
+        }
+    }
+
+    public static void info() {
+        int freePlace = 6 - countPlayers;
+
+        if (freePlace > 0) {
+            System.out.print("Команды неполные. На поле есть еще ");
+
+            switch (freePlace) {
+                case 1:
+                    System.out.println(freePlace + " свободное место");
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    System.out.println(freePlace + " свободных места");
+                    break;
+                case 5:
+                    System.out.println(freePlace + " свободных мест");
+                    break;
+            }
+        } else {
+            System.out.println("На поле нет свободных мест");
+        }
+    }
+}
---
+++ src/main/java/day7/Task2.java
@@ -0,0 +1,25 @@
+package day7;
+
+import java.util.Random;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        Player player1 = new Player(Player.MAX_STAMINA - random.nextInt(11)); // between 90 and 100
+        Player player2 = new Player(Player.MAX_STAMINA - random.nextInt(11));
+        Player player3 = new Player(Player.MAX_STAMINA - random.nextInt(11));
+        Player player4 = new Player(Player.MAX_STAMINA - random.nextInt(11));
+        Player player5 = new Player(Player.MAX_STAMINA - random.nextInt(11));
+        Player player6 = new Player(Player.MAX_STAMINA - random.nextInt(11));
+
+        Player.info();
+
+        while (player3.getStamina() > 0) {
+            player3.run();
+        }
+
+        System.out.println(Player.getCountPlayers());
+
+    }
+}
